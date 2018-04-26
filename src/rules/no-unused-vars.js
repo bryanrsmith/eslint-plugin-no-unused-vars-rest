f9c8d914433e2aad3f8d067c2ea1aca3df8a95b4
江珊 <jiangshan@77ircloud.com>
2018-04-26 11:26:33 +0800

    Instance created by freezed object should define own property by Object.defineProperty

--- src/rules/no-unused-vars.js
+++ src/rules/no-unused-vars.js
@@ -1,41 +1,50 @@
-import noUnusedVars from 'eslint/lib/rules/no-unused-vars';
+import noUnusedVars from "eslint/lib/rules/no-unused-vars";
 
 const { meta } = noUnusedVars;
 
-meta.schema[0].oneOf.find(x => x.type === 'object').properties.ignoreDestructuredVarsWithRest = {
-	type: 'boolean',
+meta.schema[0].oneOf.find(
+  x => x.type === "object"
+).properties.ignoreDestructuredVarsWithRest = {
+  type: "boolean"
 };
 
 export default {
-	meta,
-	create(context) {
-		const [ options ] = context.options;
+  meta,
+  create(context) {
+    const [options] = context.options;
 
-		let proxyContext = context;
-		if (typeof options === 'object' && options.ignoreDestructuredVarsWithRest) {
-			// ESLint freezes context, so can't monkey patch directly
-			proxyContext = Object.create(context);
-			proxyContext.report = function report(...args) {
-				const node = args.length === 1 ? args[0].node : args[0];
+    let proxyContext = context;
+    if (typeof options === "object" && options.ignoreDestructuredVarsWithRest) {
+      // ESLint freezes context, so can't monkey patch directly
+      proxyContext = Object.create(context);
+      Object.defineProperty(proxyContext, "report", {
+        value: function report(...args) {
+          const node = args.length === 1 ? args[0].node : args[0];
 
-				if (isDestructuredVarWithRestProperty(node)) {
-					// ignore reports for nodes passing the test
-					return;
-				}
+          if (isDestructuredVarWithRestProperty(node)) {
+            // ignore reports for nodes passing the test
+            return;
+          }
 
-				context.report(...args);
-			};
-		}
+          context.report(...args);
+        },
+        writable: true,
+        enumerable: true,
+        configurable: true
+      });
+    }
 
-		// create the core rule with the patched context
-		return noUnusedVars.create(proxyContext);
-	},
+    // create the core rule with the patched context
+    return noUnusedVars.create(proxyContext);
+  }
 };
 
 function isDestructuredVarWithRestProperty(node) {
-	const { parent } = node;
-	return parent &&
-		parent.type === 'Property' &&
-		parent.parent.type === 'ObjectPattern' &&
-		parent.parent.properties.some(p => p.type === 'ExperimentalRestProperty');
+  const { parent } = node;
+  return (
+    parent &&
+    parent.type === "Property" &&
+    parent.parent.type === "ObjectPattern" &&
+    parent.parent.properties.some(p => p.type === "ExperimentalRestProperty")
+  );
 }
